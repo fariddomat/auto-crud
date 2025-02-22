570713c6d7ed3a708723ca644b78090ac60a52e4
Farid Domat <fariddomat.000@gmail.com>
2025-02-22 12:37:36 +0300

    fixes

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,19 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\\{$name}Controller;
-
-use App\Http\Controllers\Dashboard\\{$name}Controller;
 Route::get('auto-crud-test', function () {
     return response()->json(['message' => 'Auto CRUD package is working!']);
 });
 
 Route::middleware(['auth'])->group(function () {
-    Route::resource('{$name}', {$name}Controller::class);
+    
+    Route::resource('{$name}', 'App\Http\Controllers\\' . ucfirst($name) . 'Controller');
 });
 
 
 Route::middleware(['auth'])->prefix('dashboard')->group(function () {
-    Route::resource('{$name}', {$name}Controller::class);
+    Route::resource('{$name}', 'App\Http\Controllers\Dashboard\\' . ucfirst($name) . 'Controller');
+
 });
 
